85376506e73ad7b53a6bfb7e3c3a18fcaea2ae93
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2019-05-10 16:18:36 -0700

    Add some code

--- src/main/java/test_java_repo/Main.java
+++ src/main/java/test_java_repo/Main.java
@@ -1,8 +1,18 @@
 package test_java_repo;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+
 public class Main {
   public static void main(String[] args) {
     Library.execute();
+    Stripe.getApiBase();
+
+    try {
+      Charge.create(null);
+    } catch (StripeException e) {
+    }
   }
 
   public static int get() { return 2; }
