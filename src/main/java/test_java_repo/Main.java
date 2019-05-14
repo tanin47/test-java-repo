f6dee0110a9b1319accc3ab435e2ad9f3870776c
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2019-05-13 18:49:04 -0700

    Update Main.java

--- src/main/java/test_java_repo/Main.java
+++ src/main/java/test_java_repo/Main.java
@@ -8,7 +8,7 @@ public class Main {
   public static void main(String[] args) {
     Stripe.getApiBase();
 
-    Library.execute();
+    Library l = new Library();
 
     try {
       Charge.create(null);
