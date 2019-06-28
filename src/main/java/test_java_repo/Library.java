7a367b50ce2b036be4bbe50ffb27e727598f104a
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2019-06-28 08:47:09 -0700

    Add a complex example

--- src/main/java/test_java_repo/Library.java
+++ src/main/java/test_java_repo/Library.java
@@ -2,4 +2,48 @@ package test_java_repo;
 
 public class Library {
   static void execute() {}
+
+  class Inner {
+    Inner2 getInner2() { return null; }
+  }
+
+  class Inner2 {
+    void inner2() {}
+  }
+
+  class Test<A> {}
+
+  class TestInner extends Test<Inner> {
+  }
+
+  class Splitor<B> {}
+
+  interface Fn<C, D> {
+    D run(C c);
+  }
+
+  class BaseStream<Z> {
+    Z onClose() { return null; }
+  }
+
+  class Stream<E> extends BaseStream<Stream<E>> {
+    E e;
+    <F> Stream<F> map(Fn<? super E, ? extends F> fn) { return null; }
+  }
+
+  <G> Stream<G> wrap(Splitor<G> s) { return null; }
+
+  <H> Splitor<H> run(Test<? extends H> t) { return null; }
+
+  void method() {
+    TestInner t = null;
+    wrap(run(t))
+      .onClose()
+      .map(a -> {
+        int x = 0;
+        return a.getInner2();
+      })
+      .e
+      .inner2();
+  }
 }
