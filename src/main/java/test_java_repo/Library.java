1009be88378ff94a4ed791ce9d8092b942cde7f3
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2019-07-06 18:01:27 -0700

    Show complex example

--- src/main/java/test_java_repo/Library.java
+++ src/main/java/test_java_repo/Library.java
@@ -1,5 +1,31 @@
 package test_java_repo;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class Library {
-  static void execute() {}
+  interface Collector<T, A, R> {}
+
+  static class Person {
+    String getName() { return null; }
+  }
+
+  static <T, K, D, A, M extends Map<K, D>> Collector<T, ?, M> groupingBy(
+    Function<? super T, ? extends K> classifier,
+    Supplier<M> mapFactory,
+    Collector<? super T, A, D> downstream
+  ) {
+    return null;
+  }
+
+  static void execute() {
+    Collector<Person, ?, Long> collector = null;
+    groupingBy(
+      Person::getName,
+      HashMap::new,
+      collector
+    );
+  }
 }
